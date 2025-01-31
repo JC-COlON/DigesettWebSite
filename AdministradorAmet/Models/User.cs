ba5375fe0f5697deb0bd481645f2f2510b91ae03
Leonardo Junior Colon <2019-0008@unad.edu.do>
2025-01-30 23:17:29 -0400

    Editar usuarios ready

--- AdministradorAmet/Models/User.cs
+++ AdministradorAmet/Models/User.cs
@@ -9,6 +9,7 @@ namespace AdministradorAmet.Models
         public string Cedula { get; set; }
         public string Name { get; set; }
         public string LastName { get; set; }
+        public string Password { get; set; }
         public string? email { get; set; }
         public string? Phone { get; set; }
         public string? profileImg { get; set; }
@@ -16,13 +17,21 @@ namespace AdministradorAmet.Models
         public string? Gender { get; set; }
         public string? bloodType { get; set; }
         public string? workLocation { get; set; }
-        public string? civilStatus { get; set; }
+        public string? CivilStatus { get; set; }
         public string? Role { get; set; }
         public string? status { get; set; }
         public string? birthdate { get; set; }
         public double? height { get; set; }
         public string? NoAgente { get; set; }
         public string FullName => $"{Name} {LastName}";
+
+
+        //public int bloodTypee { get; set; }
+        //public int heightt { get; set; }
+        //public DateTime birthdatee { get; set; }
+        //public int civilStatuss { get; set; }
+        //public int workLocationn { get; set; }
+        //public int Genderr { get; set; }
 
     }
 }
